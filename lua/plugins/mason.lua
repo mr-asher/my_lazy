f2ef8bcb6037e0574efb29d0c671e2daf95af0a1
Asher <jasherleslie@gmail.com>
2023-11-14 22:49:06 +1100

    Add lsp installs.

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,20 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "flake8",
+      "black",
+      "curlylint",
+      "djlint",
+      "eslint_d",
+      "flake8",
+      "lua-language-server",
+      "mypy",
+      "prettier",
+      "pydocstyle",
+      "pyproject-flake8",
+    },
+  },
+}
