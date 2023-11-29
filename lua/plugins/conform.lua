26dd0a22a04d8e07c92a64ac7f73d14d2abaac74
Asher <jasherleslie@gmail.com>
2023-11-30 10:48:02 +1100

    Add stuff to conform.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,7 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { "black" },
+      python = { "black", "isort" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       html = { "prettier" },
